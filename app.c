06cff03a59fc8909074ab24068179977d3aeb39f
Ali Salem <ali.salem.en@gmail.com>
2020-09-27 10:57:43 +0100

    second commit

--- app.c
+++ app.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 int main() {
    //printf() displays the string inside quotation
-       while(1) {
-	printf("Hello, DevOps! \n");
-	sleep(3);
+	for (int i = 1; i =< 10; i++)
+	{
+		printf("Hello DevOps !! \n");
+		sleep(3)
+	}
+  return 0;
 }
-          return 0;
-          }
