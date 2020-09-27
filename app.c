88e34465422dda5fad56534c2d22ffa71dd1cd50
Ali Salem <ali.salem.en@gmail.com>
2020-09-27 10:05:53 +0100

    first commit

---
+++ app.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+   //printf() displays the string inside quotation
+       while(1) {
+	printf("Hello, DevOps! \n");
+	sleep(3);
+}
+          return 0;
+          }
